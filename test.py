7990eef42ed60d801009cde2cba13e88d54eb507
daviguima <dvdgmf@gmail.com>
2019-12-03 22:58:49 +0100

    added virtual env

--- test.py
+++ test.py
@@ -1,31 +1,31 @@
-from matplotlib import colors
+from matplotlib import colors #ok
 from osgeo import gdal
 from osgeo import osr
-from pathlib import Path
+from pathlib import Path #ok
 from PyPDF2 import PdfFileMerger
 from scipy.spatial import ConvexHull, Delaunay
 from shutil import copy
-from sklearn import cluster
-from sklearn import metrics
-from sklearn import preprocessing
-from sklearn.cross_validation import cross_val_score
-from sklearn.decomposition import PCA
-from sklearn.grid_search import GridSearchCV
-from sklearn.mixture import GMM
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neural_network import MLPClassifier
-from sklearn.svm import LinearSVC
-from unittest import TestCase
-import argparse
-import ast
-import configparser
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
-import os
-import pysptools.abundance_maps as abundance_maps
-import sys
+from sklearn import cluster #ok
+from sklearn import metrics #ok
+from sklearn import preprocessing #ok
+from sklearn.cross_validation import cross_val_score #ok
+from sklearn.decomposition import PCA #ok
+from sklearn.grid_search import GridSearchCV #ok
+from sklearn.mixture import GMM #ok
+from sklearn.model_selection import train_test_split #ok
+from sklearn.naive_bayes import GaussianNB #ok
+from sklearn.neural_network import MLPClassifier #ok
+from sklearn.svm import LinearSVC #ok
+from unittest import TestCase #ok
+import argparse #ok
+import ast #ok
+import configparser #ok
+import matplotlib #ok
+import matplotlib.pyplot as plt #ok
+import numpy as np #ok
+import os #ok
+import pysptools.abundance_maps as abundance_maps #ok
+import sys #ok
 matplotlib.use('Agg')
 
 if __name__ == '__main__':
