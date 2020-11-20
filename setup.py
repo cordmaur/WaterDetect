5e8121604b2a4b653ee1e99b3b54e30d427cae98
David Franca <dvdgmf@gmail.com>
2020-11-20 14:47:24 +0100

    updating gitignore

--- setup.py
+++ setup.py
@@ -4,18 +4,11 @@ import setuptools
 short_description = 'WaterDetect generates open water cover mask for L2A Sentinel 2 imagery without any a priori knowledge on the scene.'\
                     ' It can also be used for Landsat 8 images and for other multispectral clustering/segmentation tasks.'
 
-# try:
-#     # noinspection PyPackageRequirements
-#     import pypandoc
-#
-#     long_description = pypandoc.convert('README.md', 'rst')
-# except RuntimeError:
-
 long_description = short_description
 
 setuptools.setup(
     name="waterdetect", # Replace with your own username
-    version="1.2",
+    version="1.3",
     author="Maurício Cordeiro",
     author_email="cordmaur@gmail.com",
     description=short_description,
@@ -38,8 +31,6 @@ setuptools.setup(
         'numpy>=1.17',
         'scikit_learn>=0.23',
         'matplotlib>=3.3',
-        'PyPDF2>=1.26',
-        'scikit-image>=0.16',
-        'GDAL>=3.0.2',
+        'PyPDF2>=1.26'
     ]
 )
