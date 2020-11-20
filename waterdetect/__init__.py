af25676ce0375a853d5170ca0704dcfc621a4ca8
Mauricio Cordeiro <cordmaur@gmail.com>
2020-11-20 15:43:52 +0100

    Tested MAJA and L1C from command prompt

--- waterdetect/__init__.py
+++ waterdetect/__init__.py
@@ -1,11 +1,13 @@
-#todo: Usar o Logging
+# todo: Implement logging
 # import logging
+
 
 class DWProducts:
     Landsat8_USGS = 'L8_USGS'
     Sentinel2_THEIA = 'S2_THEIA'
     Sentinel2_ESA = 'S2_S2COR'
     Sentinel2_L1C = 'S2_L1C'
+
 
 try:
     from osgeo import gdal
